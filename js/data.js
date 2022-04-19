5ce09b288651a4683c1170c93efa42535cffa0b3
pagatelabirra <raulmatiascabral@gmail.com>
2022-04-18 23:06:46 -0300

    Cambio lower to uppercase en formato de imagen

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 const productos = [
-    {"id":"1", "name":"Bombachita de Nena", "price": 1800, "discount":0, "src1":"/resources/img/product-images/bombachita1.jpg", "src2": "/resources/img/product-images/bombachita2.jpg", "quantity" : "1"},
-    {"id":"2", "name":"Boxer Algodon de Hombre", "price": 3200, "discount":20, "src1":"/resources/img/product-images/boxer1.jpg", "src2":"/resources/img/product-images/boxer2.jpg", "quantity" : "1"},
-    {"id":"3", "name":"Calzoncillos Slip de Niño", "price": 1800, "discount":5, "src1":"/resources/img/product-images/calzoncillo2.jpg", "src2":"./resources/img/product-images/calzoncillo1.jpg", "quantity" : "1"},
+    {"id":"1", "name":"Bombachita de Nena", "price": 1800, "discount":0, "src1":"/resources/img/product-images/bombachita1.JPG", "src2": "/resources/img/product-images/bombachita2.jpg", "quantity" : "1"},
+    {"id":"2", "name":"Boxer Algodon de Hombre", "price": 3200, "discount":20, "src1":"/resources/img/product-images/boxer1.JPG", "src2":"/resources/img/product-images/boxer2.jpg", "quantity" : "1"},
+    {"id":"3", "name":"Calzoncillos Slip de Niño", "price": 1800, "discount":5, "src1":"/resources/img/product-images/calzoncillo2.JPG", "src2":"./resources/img/product-images/calzoncillo1.jpg", "quantity" : "1"},
     {"id":"4", "name":"Medias Caña de Hombre", "price": 850, "discount":0, "src1":"/resources/img/product-images/media-h2.jpg", "src2":"/resources/img/product-images/media-h1.jpg", "quantity" : "1"},
     {"id":"5", "name":"Soquetes de Hombre", "price": 780, "discount":0, "src1":"/resources/img/product-images/soquete-h1.jpg", "src2":"/resources/img/product-images/soquete-h2.jpg", "quantity" : "1"},
     {"id":"6", "name":"Medias Caña de Niño", "price": 780, "discount":0, "src1":"/resources/img/product-images/media-nino.jpg", "src2":"/resources/img/product-images/media-nino.jpg", "quantity" : "1"},
